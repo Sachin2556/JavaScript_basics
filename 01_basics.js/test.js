bcdc8ded368c6adc2e9d485a789ee833521be528
Sachin2556 <151154300+Sachin2556@users.noreply.github.com>
2024-08-12 07:14:45 +0000

    update code

--- 01_basics.js/test.js
+++ 01_basics.js/test.js
@@ -1,1 +1,60 @@
 console.log("Sachin")
+
+
+let x="10abc";
+y=Number(x);
+// console.log(typeof(y))
+// console.log(y)
+
+let p=null
+// console.log(typeof(p))
+
+
+//              ********    Operations   ******
+
+
+let value=3;
+let negval=-3;
+// console.log(negval)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)   // ** means power of any number
+// console.log(2/2)
+// console.log(2%2)
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+
+//    ******    Comparisons    *******
+
+      //  Equality check (==)  is totally different from comparisons operators(>=, < ,>, <= )
+
+    //   here null convert in 0 number 
+
+
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null<=0)
+
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined<=0)
+
+
+ //   == and === both are different. Triple equal to check strictly (both are same value and both are same data types)
+      
+         console.log("2"==2)
+         console.log("2"===2)   // it checks  datatypes also
+
+         
